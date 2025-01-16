c6d6434d921a0cc65cd78c989310096c223f97ba
templatesschools <banglacomedystore@gmail.com>
2025-01-16 23:52:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("mobileMenuShow").style.height = "600px";
+}
+
+function closeMobileMenu() {
+    document.getElementById("mobileMenuShow").style.height = "0px";
+}
